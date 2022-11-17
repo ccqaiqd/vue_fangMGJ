0a78313925e14c685fa64e5e067ca76d01b35041
Cheng Chaoquan <1966024859@qq.com>
2022-11-17 10:57:23 +0800

    优化完善视频直播右侧爱心及其头像样式

--- src/config/lazy-load.js
+++ src/config/lazy-load.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import { Dialog, Picker, Swiper } from '@nutui/nutui'
+import { Dialog, Picker, Swiper, Avatar } from '@nutui/nutui'
 
 Dialog.install(Vue)
 Picker.install(Vue)
 Swiper.install(Vue)
+Avatar.install(Vue)
--- src/views/test/onRightSideTheList/OnRightSideTheList.vue
+++ src/views/test/onRightSideTheList/OnRightSideTheList.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="onRightSideTheList">
     <div class="onRightSideTheListItem">
-      <slot name="headPortrait">头像</slot>
+      <nut-avatar
+        style="display: block; margin: 4px; width: 45px; height: 45px"
+        bg-icon
+        bg-image="http://img30.360buyimg.com/uba/jfs/t1/84318/29/2102/10483/5d0704c1Eb767fa74/fc456b03fdd6cbab.png"></nut-avatar>
+      <!-- <slot name="headPortrait">头像</slot> -->
     </div>
-    <div class="onRightSideTheListItem">
+    <div
+      class="onRightSideTheListItem"
+      @click="love(isLoveFlag)">
       <img
+        v-if="isLoveFlag"
         src="@/assets/img/live/love.svg"
         alt="" />
-      <slot name="love">爱心</slot>
+      <img
+        v-else
+        src="@/assets/img/live/love2.svg"
+        alt="" />
+      <div name="love">{{ loveNum }}</div>
     </div>
     <div class="onRightSideTheListItem">
       <img
@@ -27,6 +38,22 @@
 <script>
   export default {
     name: 'OnRightSideTheList',
+    data() {
+      return {
+        isLoveFlag: true,
+        loveNum: 666,
+      }
+    },
+    methods: {
+      love(isLoveFlag) {
+        this.isLoveFlag = !this.isLoveFlag
+        if (isLoveFlag == true) {
+          this.loveNum += 1
+        } else {
+          this.loveNum -= 1
+        }
+      },
+    },
   }
 </script>
 
@@ -37,17 +64,18 @@
     align-items: center;
     width: 70px;
     height: 250px;
-    background-color: skyblue;
+    /* background-color: skyblue; */
     z-index: 9999;
   }
   .onRightSideTheListItem {
     padding-top: 20px;
     flex: 1;
+    color: white;
   }
   .onRightSideTheListItem img {
     margin-bottom: 3px;
     display: block;
-    width: 35px;
-    height: 35px;
+    width: 30px;
+    height: 30px;
   }
 </style>
