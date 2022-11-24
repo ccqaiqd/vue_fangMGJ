404b8967cab78e1b658e33de9a1909d8d59f4753
chengchaoquan <1966024859@qq.com>
2022-11-24 22:27:40 +0800

    1、对better-scroll滚动插件的组件化封装（方便使用，方便后期更改） 2、home页使用Scroll这个组件

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- 滾動插件 BScroll 的二次封装使用 -->
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll"
+export default {
+  naem:'Scroll',
+  data() {
+    return {
+      scroll:null,
+    }
+  },
+  // 组件挂载完毕再操作DOM元素 不然无效
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper,{
+
+    })
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,201 +1,122 @@
 <template>
-  <div>
-    <NavBar class="center"><div slot="center">购物街</div></NavBar>
-    <HomeSwiper
-      :banners="banners"
-      class="lunbo"></HomeSwiper>
-    <RecommendView :recommends="recommends"></RecommendView>
-    <FeatureView></FeatureView>
-    <TabControl
-      class="tabControl"
-      :titles="['流行', '新款', '精品']"
-      @tabClick="tabClick"></TabControl>
-    <GoodsList :goods="goods[currentType[this.indexClass]]"></GoodsList>
-    <ul>
-      <li>站位1</li>
-      <li>站位2</li>
-      <li>站位3</li>
-      <li>站位4</li>
-      <li>站位5</li>
-      <li>站位6</li>
-      <li>站位7</li>
-      <li>站位8</li>
-      <li>站位9</li>
-      <li>站位10</li>
-      <li>站位11</li>
-      <li>站位12</li>
-      <li>站位13</li>
-      <li>站位14</li>
-      <li>站位15</li>
-      <li>站位16</li>
-      <li>站位17</li>
-      <li>站位18</li>
-      <li>站位19</li>
-      <li>站位20</li>
-      <li>站位21</li>
-      <li>站位22</li>
-      <li>站位23</li>
-      <li>站位24</li>
-      <li>站位25</li>
-      <li>站位26</li>
-      <li>站位27</li>
-      <li>站位28</li>
-      <li>站位29</li>
-      <li>站位30</li>
-      <li>站位31</li>
-      <li>站位32</li>
-      <li>站位33</li>
-      <li>站位34</li>
-      <li>站位35</li>
-      <li>站位36</li>
-      <li>站位37</li>
-      <li>站位38</li>
-      <li>站位39</li>
-      <li>站位40</li>
-      <li>站位41</li>
-      <li>站位42</li>
-      <li>站位43</li>
-      <li>站位44</li>
-      <li>站位45</li>
-      <li>站位46</li>
-      <li>站位47</li>
-      <li>站位48</li>
-      <li>站位49</li>
-      <li>站位50</li>
-      <li>站位51</li>
-      <li>站位52</li>
-      <li>站位53</li>
-      <li>站位54</li>
-      <li>站位55</li>
-      <li>站位56</li>
-      <li>站位57</li>
-      <li>站位58</li>
-      <li>站位59</li>
-      <li>站位60</li>
-      <li>站位61</li>
-      <li>站位62</li>
-      <li>站位63</li>
-      <li>站位64</li>
-      <li>站位65</li>
-      <li>站位66</li>
-      <li>站位67</li>
-      <li>站位68</li>
-      <li>站位69</li>
-      <li>站位70</li>
-      <li>站位71</li>
-      <li>站位72</li>
-      <li>站位73</li>
-      <li>站位74</li>
-      <li>站位75</li>
-      <li>站位76</li>
-      <li>站位77</li>
-      <li>站位78</li>
-      <li>站位79</li>
-      <li>站位80</li>
-      <li>站位81</li>
-      <li>站位82</li>
-      <li>站位83</li>
-      <li>站位84</li>
-      <li>站位85</li>
-      <li>站位86</li>
-      <li>站位87</li>
-      <li>站位88</li>
-      <li>站位89</li>
-      <li>站位90</li>
-      <li>站位91</li>
-      <li>站位92</li>
-      <li>站位93</li>
-      <li>站位94</li>
-      <li>站位95</li>
-      <li>站位96</li>
-      <li>站位97</li>
-      <li>站位98</li>
-      <li>站位99</li>
-      <li>站位100</li>
-    </ul>
+  <div id="home">
+    <NavBar class="center">
+      <div slot="center">购物街</div>
+    </NavBar>
+    <Scroll class="content">
+      <HomeSwiper :banners="banners" class="lunbo"></HomeSwiper>
+      <RecommendView :recommends="recommends"></RecommendView>
+      <FeatureView></FeatureView>
+      <TabControl class="tabControl" :titles="['流行', '新款', '精品']" @tabClick="tabClick"></TabControl>
+      <GoodsList :goods="goods[currentType[this.indexClass]]"></GoodsList>
+    </Scroll>
+    
   </div>
 </template>
 
 <script>
-  import NavBar from '@/components/common/navbar/NavBar'
-  import HomeSwiper from '@/views/home/childComps/HomeSwiper'
-  import RecommendView from '@/views/home/childComps/RecommendView'
-  import FeatureView from '@/views/home/childComps/FeatureView'
-  import TabControl from '@/components/content/tabControl/TabControl'
-  import GoodsList from '@/components/content/goods/GoodsList'
-  import { getHomeMultidata, getHomeGoods } from '@/network/home'
+import NavBar from '@/components/common/navbar/NavBar'
+import HomeSwiper from '@/views/home/childComps/HomeSwiper'
+import RecommendView from '@/views/home/childComps/RecommendView'
+import FeatureView from '@/views/home/childComps/FeatureView'
+import TabControl from '@/components/content/tabControl/TabControl'
+import GoodsList from '@/components/content/goods/GoodsList'
+import { getHomeMultidata, getHomeGoods } from '@/network/home'
+import Scroll from '@/components/common/scroll/Scroll'
 
-  export default {
-    name: 'Home',
-    data() {
-      return {
-        banners: [],
-        recommends: [],
-        // 首页下拉商品数据模型设计
-        goods: {
-          pop: { page: 1, list: [] },
-          new: { page: 1, list: [] },
-          sell: { page: 1, list: [] },
-        },
-        // 下面两个组合起来判断出不同的数据展示  pop new sell
-        currentType: ['pop', 'new', 'sell'],
-        indexClass: 0,
-      }
+export default {
+  name: 'Home',
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      // 首页下拉商品数据模型设计
+      goods: {
+        pop: { page: 1, list: [] },
+        new: { page: 1, list: [] },
+        sell: { page: 1, list: [] }
+      },
+      // 下面两个组合起来判断出不同的数据展示  pop new sell
+      currentType: ['pop', 'new', 'sell'],
+      indexClass: 0
+    }
+  },
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl,
+    GoodsList,
+    // 滚动插件二次封装使用
+    Scroll
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      console.log('res~~~', res)
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+    // 请求不同分页商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    // 监听儿子的点击事件（tab栏里的不同数据）
+    tabClick(index, b, c) {
+      console.log('index', index, b, c)
+      this.indexClass = index
     },
-    components: {
-      NavBar,
-      HomeSwiper,
-      RecommendView,
-      FeatureView,
-      TabControl,
-      GoodsList,
-    },
-    created() {
-      getHomeMultidata().then(res => {
-        console.log('res~~~', res)
-        this.banners = res.data.banner.list
-        this.recommends = res.data.recommend.list
+    getHomeGoods(type) {
+      let page = this.goods[type].page
+      getHomeGoods(type, page).then(res => {
+        console.log(`商品列表数据${type}`, res.data)
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
       })
-      // 请求不同分页商品数据
-      this.getHomeGoods('pop')
-      this.getHomeGoods('new')
-      this.getHomeGoods('sell')
-    },
-    methods: {
-      // 监听儿子的点击事件（tab栏里的不同数据）
-      tabClick(index, b, c) {
-        console.log('index', index, b, c)
-        this.indexClass = index
-      },
-      getHomeGoods(type) {
-        let page = this.goods[type].page
-        getHomeGoods(type, page).then(res => {
-          console.log(`商品列表数据${type}`, res.data)
-          this.goods[type].list.push(...res.data.list)
-          this.goods[type].page += 1
-        })
-      },
-    },
+    }
   }
+}
 </script>
 
 <style scoped>
-  .center {
-    /* 使用定义好的常量 */
-    background-color: var(--color-tint);
-    color: white;
-    /* 阻止导航栏随着滚动而滚动 用 fixed 定位 */
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 9;
-  }
-  .lunbo {
-    padding-top: 44px;
-  }
-  .tabControl {
-    position: sticky;
-    top: 44px;
-  }
+#home {
+  /* padding-top: 44px; */
+  height: 100vh;
+  position: relative;
+}
+.center {
+  /* 使用定义好的常量 */
+  background-color: var(--color-tint);
+  color: white;
+  /* 阻止导航栏随着滚动而滚动 用 fixed 定位 */
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+.lunbo {
+  padding-top: 44px;
+}
+.tabControl {
+  position: sticky;
+  top: 44px;
+}
+.content {
+  /* height: 300px; */
+  /* background-color: red; */
+  overflow: hidden;
+  /* 通过定位来控制可控滚动区域  */
+  
+  /* 动态确定高度 可以使用定位  也可以用 calc() 这个c3函数来动态计算在不同屏幕下的高度 */
+  position:absolute;
+  /* top: 44px; */
+  top: 7px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+
+}
 </style>
